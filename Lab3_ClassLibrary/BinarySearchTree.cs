3f99b293952815eaa2c94cae134c377f76ee544c
Benjamin Allen <ben.allen.02.11@gmail.com>
2024-11-27 23:21:45 -0500

    Add project files.

---
+++ Lab3_ClassLibrary/BinarySearchTree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_ClassLibrary
+{
+    internal class BinarySearchTree
+    {
+        private TreeNode root;
+
+        public TreeNode Root => root;
+
+        public BinarySearchTree(List<> games)
+        {
+            games.Sort();
+
+            root = Create(0, games.Count - 1, games);
+        }
+
+        private TreeNode Create(int min, int max, List<> games)
+        {
+            // return null if out of scope of the List
+            if (max < min) return null;
+
+            // Find the middle between the two indexes min and max
+            int mid = (int)Math.Floor(((double)min + (double)max) / 2.0);
+
+            // Create a node with the value at the mid index
+            TreeNode newNode = new TreeNode(games[mid]);
+
+            // Create the left reference
+            newNode.left = Create(min, mid - 1, games);
+
+            // Create the right reference
+            newNode.right = Create(mid + 1, max, games);
+
+            // Return the node that was created
+            return newNode;
+        }
+    }
+}
---
+++ Lab3_ClassLibrary/VideoGame.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_ClassLibrary
+{
+    public class VideoGame : IComparable<VideoGame>, IEquatable<VideoGame>
+    {
+        // Fields
+        private string title;
+        private string genre;
+        private string developer;
+        private DateTime releaseDate;
+        private string platform;
+
+        // Properties
+        public string Title { get { return title; } set { title = value; } }
+        public string Genre { get { return genre; } set { genre = value; } }
+        public string Developer { get { return developer; } set { developer = value; } }
+        public DateTime ReleaseDate { get { return releaseDate; } set { releaseDate = value; } }
+        public string Platform { get { return platform; } set { platform = value; } }
+
+        // Constructor
+        public VideoGame(string title, string genre, string developer, DateTime releaseDate, string platform)
+        {
+            Title = title;
+            Genre = genre;
+            Developer = developer;
+            ReleaseDate = releaseDate;
+            Platform = platform;
+        }
+
+
+        public override string ToString() => 
+            $"{Title}\nGenre: {Genre}\nDeveloper: {Developer}\nRelease Date: {ReleaseDate.ToShortDateString()}\nPlatform: {Platform}";
+        
+        public int CompareTo(VideoGame other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Equals(VideoGame other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
